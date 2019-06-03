95aef958ca1e595fea9654665dc92a77a7951c64
Moe Maysami <mohammad.maysami@gmail.com>
2019-06-02 20:04:41 -0400

    DB176

---
+++ DB176_Second_Highest_Salary/DB176_Solutions.sql
@@ -0,0 +1,10 @@
+/* 
+176. Second Highest Salary
+https://leetcode.com/problems/second-highest-salary/ 
+*/
+
+-- Solution A (Fails for Null)
+select top(1) Salary as SecondHighestSalary from 
+(select top(2) Salary from Employee order by Salary Desc) as TopTable
+order by Salary Asc
+
