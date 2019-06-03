0757bd6c41c34f2bc9166de92661a05ae8299d08
Moe Maysami <mohammad.maysami@gmail.com>
2019-06-02 20:05:27 -0400

    DB176 Generalized Solution

--- DB176_Second_Highest_Salary/DB176_Solutions.sql
+++ DB176_Second_Highest_Salary/DB176_Solutions.sql
@@ -4,7 +4,13 @@ https://leetcode.com/problems/second-highest-salary/
 */
 
 -- Solution A (Fails for Null)
-select top(1) Salary as SecondHighestSalary from 
-(select top(2) Salary from Employee order by Salary Desc) as TopTable
-order by Salary Asc
+-- select top(1) Salary as SecondHighestSalary from 
+-- (select top(2) Salary from Employee order by Salary Desc) as TopTable
+-- order by Salary Asc
+
+
+-- Solution B
+select max(Salary) as SecondHighestSalary from Employee 
+where Salary < (select Max(Salary) from Employee);
+
 
