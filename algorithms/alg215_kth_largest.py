003fa58dc4b804f18b619cfa5f7c69e81d4d5dac
Moe Maysami <moe.maysami@nielsen.com>
2022-01-03 00:59:46 -0500

    Add More Problems and Tools

---
+++ algorithms/alg215_kth_largest.py
@@ -0,0 +1,133 @@
+# 215. Kth Largest Element in an array [FB]
+# https://leetcode.com/problems/kth-largest-element-in-an-array/
+#
+# Given an integer array nums and an integer k, return the kth largest element in the array.
+# Note that it is the kth largest element in the sorted order, not the kth distinct element.
+#
+# Example:
+#   Input: nums = [3,2,1,5,6,4], k = 2
+#   Output: 5
+#
+#   Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+#   Output: 4
+#
+# Constraints:
+#     1 <= k <= nums.length <= 104
+#     -104 <= nums[i] <= 104
+
+import typing
+import heapq
+
+
+class Solution:
+    def findKthLargest(self, nums: list, k: int) -> int:
+        """
+        heapq is a binary heap, O(N) heapify, O(log n) push and O(log n) pop. See the heapq source code.
+        https://stackoverflow.com/questions/38806202/whats-the-time-complexity-of-functions-in-heapq-library/38833175
+        """
+        # K-Restricted heapq
+        # time complexity: O(N * log K), space : O(K)
+
+        if not nums or not (0 < k <= len(nums)): return -1
+        q = nums[:k]
+        heapq.heapify(q)
+
+        for num in nums[k::]:
+            # print('element {:2},  heap: {}'.format(num, q))
+            heapq.heappushpop(q, num)
+
+        # Root = K-th Smallest Number 
+        return q[0]
+
+    def findKthLargest_heap(self, nums: list, k: int) -> int:
+        """
+        This is not a solution but a proxy to find array of K largest elements
+        Time  Complexity O(N+K LogN)
+        Space Complexity O(K)
+
+        :param k:
+        :return:
+        """
+
+        if not nums or not (0 < k <= len(nums)): return -1
+        temp = []
+        nums_neg = [-e for e in nums]
+        heapq.heapify(nums_neg)  # O(n)
+        for i in range(k):
+            temp.append(-heapq.heappop(nums_neg))  # O(K LogN)
+
+        return temp[-1]
+
+    def findKthLargest_selection(self, nums: list, k: int) -> int:
+        """
+        Use Selection Sort for loop K times
+
+        Time Complexity O(K.N)
+        Space Complexity O(1)
+
+        :param k:
+        :return:
+        """
+
+        if not nums or not (0 < k <= len(nums)): return -1
+
+        for i in range(k):  # O(K)
+            j_max = i
+            for j in range(i + 1, len(nums)):  # O(N)
+                if nums[j] > nums[j_max]:
+                    j_max = j
+
+            nums[i], nums[j_max] = nums[j_max], nums[i]
+
+        return nums[k - 1]
+
+    def findKthLargest_bubble(self, nums: list, k: int) -> int:
+        """
+        Use Bubble Sort while running outer look K times
+
+        Time Complexity O(K.N)
+        Space Complexity O(1)
+
+        :param k:
+        :return:
+        """
+        if not nums or not (0 < k <= len(nums)): return -1
+
+        for i in range(k):  # O(K)
+            for j in range(len(nums) - 1):  # O(N)
+                if nums[j] > nums[j + 1]:
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+
+        return nums[-k]
+
+
+# ===============================================================================
+# ===============================================================================
+# ===============================================================================
+if __name__ == "__main__":
+    sol = Solution()
+    my_in = [13, 2, 21, 8]
+    k = 2
+    out = 13
+
+    d1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+    d2 = d1[::-1]
+    d3 = [3, 1, 9, 7, -2, 14, 9, 8, 7, 6]
+    d4 = [-9, 1, 2, -4, 3, -2, -14, 0, 2, 3, 6]
+
+    k = 7
+    cases = [d1, d2, d3, d4]
+
+    for d in cases:
+        kth_selection = sol.findKthLargest_selection(d, k)
+        kth_bubble = sol.findKthLargest_bubble(d, k)
+        kth_heap = sol.findKthLargest_heap(d, k)
+        kth_out = sol.findKthLargest(d, k)
+
+        print("\nInput  : %s" % d)
+        print("Expected : %s" % sorted(d)[-k])
+        print('---')
+        print('Top K-SEL: %s' % kth_selection)
+        print('Top K-BBL: %s' % kth_bubble)
+        print('Top K-HIP: %s' % kth_heap)
+        print('Top K-MHP: %s' % kth_out)
---
+++ algorithms/alg957_prison_cells.py
@@ -0,0 +1,102 @@
+# 957. Prison Cells After N Days
+# https://leetcode.com/problems/prison-cells-after-n-days/
+#
+# here are 8 prison cells in a row, and each cell is either occupied or vacant.
+# Each day, whether the cell is occupied or vacant changes according to the following rules:
+#   - If a cell has two adjacent neighbors that are both occupied or both vacant, then the cell becomes occupied.
+#   - Otherwise, it becomes vacant.
+#   - That because the prison is a row, the first and the last cells in the row can't have two adjacent neighbors and will set to Empty!?
+#
+# We describe the current state of the prison in the following way: cells[i] == 1 if the i-th cell is occupied, else cells[i] == 0.
+# Given the initial state of the prison, return the state of the prison after N days (and N such changes described above.)
+#
+# Constraints:
+#     cells.length == 8
+#     cells[i] is in {0, 1}
+#     1 <= N <= 10^9
+#
+# Examples:
+#
+#   Input:  [0,1,0,1,1,0,0,1], N = 7
+#   Output: [0,0,1,1,0,0,0,0]
+#       Day 0: [0, 1, 0, 1, 1, 0, 0, 1]
+#       Day 1: [0, 1, 1, 0, 0, 0, 0, 0]
+#       Day 2: [0, 0, 0, 0, 1, 1, 1, 0]
+#       Day 3: [0, 1, 1, 0, 0, 1, 0, 0]
+#       Day 4: [0, 0, 0, 0, 0, 1, 0, 0]
+#       Day 5: [0, 1, 1, 1, 0, 1, 0, 0]
+#       Day 6: [0, 0, 1, 0, 1, 1, 0, 0]
+#       Day 7: [0, 0, 1, 1, 0, 0, 0, 0]
+#
+#   Input:  [1,0,0,1,0,0,1,0], N = 1000000000
+#   Output: [0,0,1,1,1,1,1,0]
+
+
+class Solution:
+    """
+    #   Input:  [0,1,0,1,1,0,0,1], N = 7
+    #   Output: [0,0,1,1,0,0,0,0]
+    #       Day 0:  [0, 1, 0, 1, 1, 0, 0, 1]
+    #       Day 1:  [0, 1, 1, 0, 0, 0, 0, 0]    Cycle
+    #       Day 2:  [0, 0, 0, 0, 1, 1, 1, 0]
+    #       Day 3:  [0, 1, 1, 0, 0, 1, 0, 0]
+    #       Day 4:  [0, 0, 0, 0, 0, 1, 0, 0]
+    #       Day 5:  [0, 1, 1, 1, 0, 1, 0, 0]
+    #       Day 6:  [0, 0, 1, 0, 1, 1, 0, 0]
+    #       Day 7:  [0, 0, 1, 1, 0, 0, 0, 0]
+    #       Day 8:  [0, 0, 0, 0, 0, 1, 1, 0]
+    #       Day 9:  [0, 1, 1, 1, 0, 0, 0, 0]
+    #       Day 10: [0, 0, 1, 0, 0, 1, 1, 0]
+    #       Day 11: [0, 0, 1, 0, 0, 0, 0, 0]
+    #       Day 12: [0, 0, 1, 0, 1, 1, 1, 0]
+    #       Day 13: [0, 0, 1, 1, 0, 1, 0, 0]
+    #       Day 14: [0, 0, 0, 0, 1, 1, 0, 0]
+    #       Day 15: [0, 1, 1, 0, 0, 0, 0, 0]    Cycle
+    """
+
+    # def prisonAfter1Day(self, cells: list) -> list:
+    #     return [0] + [1 if cells[i - 1] == cells[i + 1] else 0 for i in range(1, len(cells) - 1)] + [0]
+
+    def prisonAfterNDays(self, cells: list, n: int) -> list:
+        if n == 0:
+            return cells
+
+        cycle_end = None
+        status = cells
+        d = {str(status): 0}
+
+        for m in range(1, n + 1):
+            status = [0] + [1 if status[i - 1] == status[i + 1] else 0 for i in range(1, len(status) - 1)] + [0]
+            if not str(status) in d.keys():
+                d[str(status)] = m
+            else:
+                cycle_start, cycle_end = d[str(status)], m
+                break
+
+        if cycle_end:
+            for c in range((n - cycle_start) % (cycle_end - cycle_start)):
+                status = [0] + [1 if status[i - 1] == status[i + 1] else 0 for i in range(1, len(status) - 1)] + [0]
+        return status
+
+
+# ===============================================================================
+# ===============================================================================
+# ===============================================================================
+if __name__ == "__main__":
+    sol = Solution()
+    N1 = 7
+    in1 = [0, 1, 0, 1, 1, 0, 0, 1]
+    out1 = [0, 0, 1, 1, 0, 0, 0, 0]
+
+    N2 = 8
+    in2 = [0, 0, 1, 1, 1, 1, 0, 0]
+    out2 = [0, 0, 0, 1, 1, 0, 0, 0]
+
+    my_in, my_out, my_N = in2, out2, N2
+    out = sol.prisonAfterNDays(my_in, my_N)
+
+    print("Input    : %s" % (my_in))
+    print("N days   : %s" % (my_N))
+    print("Expected : %s" % (my_out))
+    print("Output   : %s" % (out))
+
